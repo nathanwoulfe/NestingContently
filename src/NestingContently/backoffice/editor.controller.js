8a8e4771a78b07c0219096fddc07fa0285edc7f6
Chriztian Steinmeier <chriztian@steinmeier.dk>
2020-09-12 11:43:30 +0200

    Reduce to using the native `.closest()`
    Also going all the way up right in one go.

--- src/NestingContently/backoffice/editor.controller.js
+++ src/NestingContently/backoffice/editor.controller.js
@@ -1,20 +1,12 @@
 ﻿(() => {
     // for this to work, we need a property editor, we don't, however, need it to be visible or edited...
     function nestingContently($element) {
-        const findAncestor = (el, cls) => {
-            while ((el = el.parentElement) && !el.classList.contains(cls));
-            return el;
-        }
-        
-        const propElm = findAncestor($element[0], 'umb-property');
+        const propElm = $element[0].closest('.umb-nested-content-property-container');
 
         if (propElm) {
-            var parentElem = findAncestor(propElm, 'umb-nested-content-property-container');
-            if (parentElem) {
-                parentElem.style.display = 'none';
-            }
+            propElm.style.display = 'none';
         }
-    } 
+    }
 
     angular.module('umbraco').controller('nestingContentlyController', ['$element', nestingContently]);
 })();
